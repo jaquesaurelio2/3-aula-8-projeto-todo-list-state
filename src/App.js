ccad0d98f7bebe346373a65fcda10827fd1232d1
jaquesaurelio2 <jaquesaurelio1@gmail.com>
2024-06-29 17:06:59 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/**
+  - Utilize o hook useState e crie um estado para o array de itens e inicialize-o
+  com os dados importados
+ */
+
+import * as React from "react";
+import "./styles.css";
+import Item from "./components/Item";
+import Form from "./components/Form";
+import todos from "../data";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <Form />
+      {todos.map((todo, index) => (
+        <Item key={index} title={todo.title} />
+      ))}
+    </div>
+  );
+}
